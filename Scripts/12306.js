7ba99354dae140ef8d272425334c45254df6dd1c
xp9477 <947760133@qq.com>
2024-10-23 01:22:17 +0800

    delete not used

--- Scripts/12306.js
+++ Scripts/12306.js
@@ -1,20 +1,26 @@
-// 2024-02-19 10:45
+// 12306
+// 2024-10-23 00:20:14
 
-let body = "";
+
 let obj = JSON.parse($request.body);
-const isQuanX = typeof $task !== "undefined";
 
 if (obj.placementNo === "0007") {
-  body =
-    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+  obj = {
+    "code": "00",
+    "materialsList": [
+      {
+        "billMaterialsId": "255",
+        "filePath": "h",
+        "creativeType": 1
+      }
+    ],
+    "advertParam": { "skipTime": 1 }
+  };
 } else if (obj.placementNo === "G0054") {
-  body = '{"code":"00","materialsList":[]}';
+  obj = { "code": "00", "materialsList": [] };
 } else {
-  body = '{"code":"00","message":"无广告返回"}';
+  obj = { "code": "00", "message": "无广告返回" };
 }
 
-if (isQuanX) {
-  $done({ body });
-} else {
-  $done({ response: { body } });
-}
+
+$done({body: JSON.stringify(obj)});
